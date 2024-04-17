a34452561fb2a70b0a2d444ef6c85530cc6928c5
Rithesh Pattela <134095755+Rithesh2610@users.noreply.github.com>
2024-04-17 10:43:43 -0400

    Optimized images of each Game Card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading, Image,Text } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image,Text } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 
 interface Props{
@@ -11,14 +13,16 @@ interface Props{
 const GameCard = ({game}:Props) => {
   return (
    <Card borderRadius={10} overflow='hidden'>
-    <Image src ={game.image_background}/ >
+    <Image src ={getCroppedImageUrl(game.image_background)}/ >
     <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
+        <HStack justifyContent='space-between'>
         <PlatformIconList platforms={game.parent_platform.map(p=>p.platform)}/>
-        
-    </CardBody>   
+        <CriticScore score={game.metacritic}/>
+        </HStack>
+        </CardBody>   
     </Card>
   )
 }
 
-export default GameCard
+export default GameCard;
