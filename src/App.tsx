eab56105fd7237bc06167683e9e61a9150439cbd
Rithesh Pattela <134095755+Rithesh2610@users.noreply.github.com>
2024-04-21 17:26:37 -0400

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import GenreList from './components/GenreList'
 import { Genre } from './hooks/useGenre'
 import GameGrid from './components/GameGrid'
 import Navbar from './components/NavBar'
+import PlatformSelector from './components/PlatformSelector'
+import { Platform } from './hooks/usePlatforms'
 
 function App() {
   
   const[selectedGenre,setSelectedGenre]=useState<Genre |null>(null);
+  const[selectedPlatform,setSelectedPlatform] = useState<Platform | null>(null);
   return (
     <div className="App">
       <Grid templateAreas={{base:`"nav" "aside main"`,lg : `"nav nav" "aside main"`}} templateColumns={{base :'1fr',lg:'200px 1fr'}}>
@@ -18,11 +21,12 @@ function App() {
         </GridItem>
         <Show above ="lg"> 
         <GridItem area='aside' paddingX={5} bg='gold'>
-        <GenreList onSelectedGenre={(genre)=>setSelectedGenre(genre)}/>
+        <GenreList selectedGenre ={selectedGenre} onSelectedGenre={(genre)=>setSelectedGenre(genre)}/>
         </GridItem>
         </Show>
         <GridItem area= "main" >
-          <GameGrid selectedGenre={selectedGenre} />
+          <PlatformSelector onSelectPlatform={(platform)=>setSelectedPlatform(platform)}/>
+          <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
         </GridItem>
       </Grid>
     </div>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,16 +3,22 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenre";
+import { Platform } from '../hooks/usePlatforms'
 
-const GameGrid = () => {
-      const {data,error,isLoading} =useGames();
+interface Props{
+  selectedGenre : Genre | null;
+  selectedPlatform : Platform | null;
+}
+const GameGrid = ({selectedGenre, selectedPlatform}: Props) => {
+      const {data,error,isLoading} =useGames(selectedGenre,selectedPlatform);
       const skeletons = [1,2,3,4,5,6];
     
   return (
     <>
     {error &&<Text>{error}</Text>}
-    <SimpleGrid column={{sm:1, md :2,lg:3, xl :5} }spacing={3}>
-    {isLoading && skeletons.map((skeleton)=> <GameCardContainer> <GameCardSkeleton key={skeleton}/></GameCardContainer>)}
+    <SimpleGrid column={{sm:1, md :2,lg:3, xl :5} } spacing={3}>
+    {isLoading && skeletons.map((skeleton)=> <GameCardContainer key={skeleton}> <GameCardSkeleton /></GameCardContainer>)}
     {data.map( (game) =><GameCardContainer>
       <GameCard key = {game.id} game = {game}/></GameCardContainer> )}
     </SimpleGrid>
